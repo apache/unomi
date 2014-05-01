b164edf9ea31108745c62d8340e11f1e982f8b0c
Serge Huber <shuber@jahia.com>
2014-05-01 22:22:54 +0200

    Start work on HTML and Javascript to be able to interact with REST services and perform basic tracking examples

--- wemi-context-server/wab/src/main/webapp/WEB-INF/javascript/base.js
+++ wemi-context-server/wab/src/main/webapp/WEB-INF/javascript/base.js
@@ -1,1 +1,18 @@
 // base Javascript tag container
+
+function loadXMLDoc(url, successCallBack) {
+    var xmlHttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlHttp = new XMLHttpRequest();
+    }
+    else {// code for IE6, IE5
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            successCallBack(xmlHttp);
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
