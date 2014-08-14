fd306a0a923a4df8f9b23459574929a420e479f4
Serge Huber <shuber@jahia.com>
2014-08-14 10:31:16 +0200

    - Remove login plugin since we don't need it - Switch hover and request plugins to use OSGi Blueprint

--- wemi-context-server/plugins/hover-event/src/main/java/org/oasis_open/wemi/context/server/plugins/events/hover/querybuilders/HoverEventConditionESQueryBuilder.java
+++ wemi-context-server/plugins/hover-event/src/main/java/org/oasis_open/wemi/context/server/plugins/events/hover/querybuilders/HoverEventConditionESQueryBuilder.java
@@ -5,20 +5,10 @@ import org.elasticsearch.index.query.FilterBuilders;
 import org.oasis_open.wemi.context.server.api.conditions.Condition;
 import org.oasis_open.wemi.context.server.persistence.elasticsearch.conditions.ConditionESQueryBuilderDispatcher;
 import org.oasis_open.wemi.context.server.persistence.elasticsearch.conditions.ESQueryBuilder;
-import org.ops4j.pax.cdi.api.OsgiServiceProvider;
-import org.ops4j.pax.cdi.api.Properties;
-import org.ops4j.pax.cdi.api.Property;
-
-import javax.enterprise.context.ApplicationScoped;
 
 /**
 * Created by toto on 27/06/14.
 */
-@ApplicationScoped
-@OsgiServiceProvider
-@Properties({
-    @Property(name = "queryBuilderId", value = "hoverEventConditionESQueryBuilder")
-})
 public class HoverEventConditionESQueryBuilder implements ESQueryBuilder {
 
     public HoverEventConditionESQueryBuilder() {
--- wemi-context-server/plugins/request/src/main/java/org/oasis_open/wemi/context/server/plugins/request/consequences/RequestParameterToUserPropertyConsequence.java
+++ wemi-context-server/plugins/request/src/main/java/org/oasis_open/wemi/context/server/plugins/request/consequences/RequestParameterToUserPropertyConsequence.java
@@ -4,22 +4,13 @@ import org.oasis_open.wemi.context.server.api.Event;
 import org.oasis_open.wemi.context.server.api.User;
 import org.oasis_open.wemi.context.server.api.consequences.Consequence;
 import org.oasis_open.wemi.context.server.api.consequences.ConsequenceExecutor;
-import org.ops4j.pax.cdi.api.OsgiServiceProvider;
-import org.ops4j.pax.cdi.api.Properties;
-import org.ops4j.pax.cdi.api.Property;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Copies a request parameter to a user property
  * @todo add support for multi-valued parameters or storing values as a list
  */
-@ApplicationScoped
-@OsgiServiceProvider
-@Properties({
-    @Property(name = "consequenceExecutorId", value = "requestParameterToUserProperty")
-})
 public class RequestParameterToUserPropertyConsequence implements ConsequenceExecutor {
     public boolean execute(Consequence consequence, User user, Object context) {
         boolean changed = false;
