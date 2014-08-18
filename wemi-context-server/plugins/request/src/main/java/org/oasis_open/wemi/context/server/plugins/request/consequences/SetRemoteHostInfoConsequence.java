3be490c31fdd4460e7beceee74b972e9bcef67df
Thomas Draier <tdraier@jahia.com>
2014-08-18 16:22:49 +0200

    BACKLOG-5072 : added geolocation info

--- wemi-context-server/plugins/request/src/main/java/org/oasis_open/wemi/context/server/plugins/request/consequences/SetRemoteHostInfoConsequence.java
+++ wemi-context-server/plugins/request/src/main/java/org/oasis_open/wemi/context/server/plugins/request/consequences/SetRemoteHostInfoConsequence.java
@@ -5,7 +5,13 @@ import org.oasis_open.wemi.context.server.api.Session;
 import org.oasis_open.wemi.context.server.api.consequences.Consequence;
 import org.oasis_open.wemi.context.server.api.consequences.ConsequenceExecutor;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by toto on 18/08/14.
@@ -24,6 +30,27 @@ public class SetRemoteHostInfoConsequence implements ConsequenceExecutor {
 
         session.setProperty("remoteAddr", httpServletRequest.getRemoteAddr());
         session.setProperty("remoteHost", httpServletRequest.getRemoteHost());
+        InputStream inputStream = null;
+
+        try {
+            final URL url = new URL("http://freegeoip.net/json/" + httpServletRequest.getRemoteAddr());
+            inputStream = url.openConnection().getInputStream();
+            JsonReader reader = Json.createReader(inputStream);
+            JsonObject location = (JsonObject) reader.read();
+            session.setProperty("country_code", location.getString("country_code"));
+            session.setProperty("country_name", location.getString("country_name"));
+            session.setProperty("city",location.getString("city"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         return true;
     }
