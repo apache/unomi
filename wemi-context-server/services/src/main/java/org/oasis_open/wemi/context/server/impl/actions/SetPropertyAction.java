d3bf1cf6048ddc1fd4b7fdb04d77ba85a6b2e316
Thomas Draier <tdraier@jahia.com>
2014-09-22 10:50:32 +0200

    Added scripting in setProperty action, add nbOfVisits counter

--- wemi-context-server/services/src/main/java/org/oasis_open/wemi/context/server/impl/actions/SetPropertyAction.java
+++ wemi-context-server/services/src/main/java/org/oasis_open/wemi/context/server/impl/actions/SetPropertyAction.java
@@ -1,10 +1,16 @@
 package org.oasis_open.wemi.context.server.impl.actions;
 
+import org.mvel2.MVEL;
 import org.oasis_open.wemi.context.server.api.Event;
 import org.oasis_open.wemi.context.server.api.actions.Action;
 import org.oasis_open.wemi.context.server.api.actions.ActionExecutor;
 
+import javax.script.*;
+import java.io.Serializable;
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 /**
@@ -19,12 +25,18 @@ public class SetPropertyAction implements ActionExecutor {
     }
 
     public boolean execute(Action action, Event event) {
-        String propertyValue = (String) action.getParameterValues().get("setPropertyValue");
-        if (propertyValue.equals("now")) {
+        Object propertyValue = action.getParameterValues().get("setPropertyValue");
+        if (propertyValue != null && propertyValue.equals("now")) {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
             format.setTimeZone(TimeZone.getTimeZone("UTC"));
             propertyValue = format.format(event.getTimeStamp());
+        } else if (action.getParameterValues().containsKey("script")) {
+            Map<String,Object> ctx = new HashMap<String,Object>();
+            ctx.put("session", event.getSession());
+            ctx.put("user", event.getUser());
+            propertyValue = MVEL.eval((String)action.getParameterValues().get("script"),ctx);
         }
+
         if (Boolean.TRUE.equals(action.getParameterValues().get("storeInSession"))) {
             event.getSession().setProperty(
                     (String) action.getParameterValues().get("setPropertyName"),
