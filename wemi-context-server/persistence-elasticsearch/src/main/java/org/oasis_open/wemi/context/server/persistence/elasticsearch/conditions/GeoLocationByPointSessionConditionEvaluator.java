f1b1cbd9b4f0ad07fc9f6be35d00be7e5ed8690c
Thomas Draier <tdraier@jahia.com>
2014-10-21 17:29:35 +0200

    BACKLOG-5334 : cleaned geolocation eval code

--- wemi-context-server/persistence-elasticsearch/src/main/java/org/oasis_open/wemi/context/server/persistence/elasticsearch/conditions/GeoLocationByPointSessionConditionEvaluator.java
+++ wemi-context-server/persistence-elasticsearch/src/main/java/org/oasis_open/wemi/context/server/persistence/elasticsearch/conditions/GeoLocationByPointSessionConditionEvaluator.java
@@ -5,6 +5,8 @@ import org.elasticsearch.common.geo.GeoDistance;
 import org.elasticsearch.common.unit.DistanceUnit;
 import org.oasis_open.wemi.context.server.api.Item;
 import org.oasis_open.wemi.context.server.api.conditions.Condition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
@@ -14,27 +16,23 @@ import java.util.Map;
  */
 public class GeoLocationByPointSessionConditionEvaluator implements ConditionEvaluator {
 
+    private static final Logger logger = LoggerFactory.getLogger(GeoLocationByPointSessionConditionEvaluator.class.getName());
+
     @Override
     public boolean eval(Condition condition, Item item, Map<String, Object> context, ConditionEvaluatorDispatcher dispatcher) {
-        Double latitude1 = Double.parseDouble ((String) condition.getParameterValues().get("latitude"));
-        Double longitude1 = Double.parseDouble ((String) condition.getParameterValues().get("longitude"));
-        Double latitude2 = null;
-        Double longitude2 = null;
         try {
-            latitude2 = Double.parseDouble((String) BeanUtils.getProperty(item, "properties.location.lat"));
-            longitude2 = Double.parseDouble((String) BeanUtils.getProperty(item, "properties.location.lon"));
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+            Double latitude1 = Double.parseDouble ((String) condition.getParameterValues().get("latitude"));
+            Double longitude1 = Double.parseDouble ((String) condition.getParameterValues().get("longitude"));
+            Double latitude2 = Double.parseDouble(BeanUtils.getProperty(item, "properties.location.lat"));
+            Double longitude2 = Double.parseDouble(BeanUtils.getProperty(item, "properties.location.lon"));
+            DistanceUnit.Distance distance = DistanceUnit.Distance.parseDistance((String) condition.getParameterValues().get("distance"));
+
+            double d = GeoDistance.DEFAULT.calculate(latitude1, longitude1, latitude2, longitude2, distance.unit);
+            return d < distance.value;
+        } catch (Exception e) {
+            logger.debug("Cannot get properties",e);
         }
-
-        DistanceUnit.Distance distance = DistanceUnit.Distance.parseDistance((String) condition.getParameterValues().get("distance"));
-
-        double d = GeoDistance.DEFAULT.calculate(latitude1, longitude1, latitude2, longitude2, distance.unit);
-        return d < distance.value;
+        return false;
     }
 
 
