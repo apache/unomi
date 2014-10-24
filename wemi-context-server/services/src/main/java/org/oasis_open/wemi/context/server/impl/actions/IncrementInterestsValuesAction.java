ab5485d84c844009fbf5f00eda02b765280b5962
kevan <kevan@jahia.com>
2014-10-24 18:45:47 +0200

    BACKLOG-5340: fix remaining issues with servlet refactoring

--- wemi-context-server/services/src/main/java/org/oasis_open/wemi/context/server/impl/actions/IncrementInterestsValuesAction.java
+++ wemi-context-server/services/src/main/java/org/oasis_open/wemi/context/server/impl/actions/IncrementInterestsValuesAction.java
@@ -1,6 +1,6 @@
 package org.oasis_open.wemi.context.server.impl.actions;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.oasis_open.wemi.context.server.api.Event;
 import org.oasis_open.wemi.context.server.api.actions.Action;
 import org.oasis_open.wemi.context.server.api.actions.ActionExecutor;
@@ -17,15 +17,22 @@ public class IncrementInterestsValuesAction implements ActionExecutor {
         boolean modified = false;
         Map<String, Object> userProps = event.getUser().getProperties();
 
-        for (String s : event.getProperties().keySet()) {
-            if (s.startsWith("page.interests.")) {
-                String interestName = StringUtils.substringAfter(s, "page.");
-                int value = (Integer) event.getProperty(s);
-                int oldValue = (userProps.containsKey(interestName)) ? (Integer) userProps.get(interestName) : 0;
-                event.getUser().setProperty(interestName, value + oldValue);
-                modified = true;
+        try {
+            Map<String, Object> interests = (Map<String, Object>) PropertyUtils.getProperty(event, "properties.page.interests");
+            if (interests != null) {
+                for (Map.Entry<String, Object> s : interests.entrySet()) {
+                    int value = (Integer) s.getValue();
+                    int oldValue = (userProps.containsKey(s.getKey())) ? (Integer) userProps.get(s.getKey()) : 0;
+                    event.getUser().setProperty(s.getKey(), value + oldValue);
+                    modified = true;
+                }
             }
+        } catch (UnsupportedOperationException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new UnsupportedOperationException(e);
         }
+
         return modified;
     }
 }
