5a00fb016d5f54946fc1d02e801087e865cb6a94
kevan <kevan@jahia.com>
2014-10-31 16:33:30 +0100

    BACKLOG-5367: add itest for json context request

--- wemi-context-server/itests/src/test/java/org/oasis_open/wemi/context/server/itests/BasicTest.java
+++ wemi-context-server/itests/src/test/java/org/oasis_open/wemi/context/server/itests/BasicTest.java
@@ -1,17 +1,20 @@
 package org.oasis_open.wemi.context.server.itests;
 
-import org.apache.commons.io.IOUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.oasis_open.wemi.context.server.ContextRequest;
+import org.oasis_open.wemi.context.server.ContextResponse;
 import org.oasis_open.wemi.context.server.api.Metadata;
 import org.oasis_open.wemi.context.server.api.services.SegmentService;
 import org.ops4j.pax.exam.Configuration;
@@ -28,8 +31,6 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.*;
 
 import static org.ops4j.pax.exam.CoreOptions.*;
@@ -42,11 +43,13 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRunti
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class BasicTest {
-
     private final static Logger LOGGER = LoggerFactory.getLogger(BasicTest.class);
+    private static final String JSON_MYME_TYPE = "application/json";
 
     @Inject
     protected SegmentService segmentService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Configuration
     public Option[] config() {
@@ -123,10 +126,9 @@ public class BasicTest {
     }
 
     @Test
-    public void testContext() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://localhost:8181/context.js?sessionId=aa3b04bd-8f4d-4a07-8e96-d33ffa04d3d9");
-        CloseableHttpResponse response = httpclient.execute(httpGet);
+    public void testContextJS() throws IOException {
+        HttpUriRequest request = new HttpGet("http://localhost:8181/context.js?sessionId=aa3b04bd-8f4d-4a07-8e96-d33ffa04d3d9");
+        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
         // The underlying HTTP connection is still held by the response object
         // to allow the response content to be streamed directly from the network socket.
         // In order to ensure correct deallocation of system resources
@@ -147,6 +149,30 @@ public class BasicTest {
         Assert.assertTrue("Response should contain context object", responseContent.contains("window.digitalData = window.digitalData || {};"));
         // @todo we should check the validity of the context object, but this is rather complex since it would
         // potentially require parsing the Javascript !
+    }
+
+    @Test
+    public void testContextJSON() throws IOException {
+        String sessionId = "aa3b04bd-8f4d-4a07-8e96-d33ffa04d3d9";
+        ContextRequest contextRequest = new ContextRequest();
+        contextRequest.setPageId("af6f393a-a537-4586-991b-8521b9c7b05b");
+        HttpPost request = new HttpPost("http://localhost:8181/context.json?sessionId=" + sessionId);
+        request.setEntity(new StringEntity(objectMapper.writeValueAsString(contextRequest), ContentType.create("application/json")));
+        CloseableHttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+        try {
+            // validate mimeType
+            String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+            Assert.assertEquals("Response content type should be " + JSON_MYME_TYPE, JSON_MYME_TYPE, mimeType);
+
+            // validate context
+            ContextResponse context = TestUtils.retrieveResourceFromResponse(response, ContextResponse.class);
+            Assert.assertNotNull("Context should not be null", context);
+            Assert.assertNotNull("Context userId should not be null", context.getUserId());
+            Assert.assertEquals("Context sessionId should be the same as the sessionId used to request the context", sessionId, context.getSessionId());
+        } finally {
+            response.close();
+        }
     }
 
 }
