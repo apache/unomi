55fef22405890ebcdf68a2b5578c7a3b4c30c270
Sergiy Shyrkov <sshyrkov@jahia.com>
2015-01-21 16:17:29 +0100

    DMF-97 - refactor the tests to create profile in setUp() method

--- itests/src/test/java/org/oasis_open/contextserver/itests/ConditionEvaluatorTest.java
+++ itests/src/test/java/org/oasis_open/contextserver/itests/ConditionEvaluatorTest.java
@@ -30,226 +30,183 @@ import org.ops4j.pax.exam.spi.reactors.PerSuite;
 @ExamReactorStrategy(PerSuite.class)
 public class ConditionEvaluatorTest extends BaseTest {
 
+    private ConditionBuilder builder;
+
     @Inject
     private DefinitionsService definitionsService;
 
     @Inject
     private PersistenceService persistenceService;
 
+    private Profile profile;
+
     @Before
     public void setUp() {
         assertNotNull("Definition service should be available", definitionsService);
         assertNotNull("Persistence service should be available", persistenceService);
+
+        profile = new Profile("profile-" + UUID.randomUUID().toString());
+        profile.setProperty("age", Integer.valueOf(30));
+        profile.setProperty("gender", "female");
+        profile.setProperty("lastVisit", new Date());
+        profile.setSegments(new HashSet<String>(Arrays.asList("s1", "s2")));
+
+        builder = new ConditionBuilder(definitionsService);
     }
 
     @Test
     public void testCompound() {
-        Date lastVisit = new Date();
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("lastVisit", lastVisit);
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
         // test AND
         assertTrue(persistenceService.testMatch(
                 builder.and(builder.profileProperty("properties.gender").equalTo("female"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.and(builder.profileProperty("properties.gender").equalTo("male"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.and(builder.profileProperty("properties.gender").equalTo("female"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), profile));
 
         // test OR
         assertTrue(persistenceService.testMatch(
                 builder.or(builder.profileProperty("properties.gender").equalTo("female"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.or(builder.profileProperty("properties.gender").equalTo("male"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.or(builder.profileProperty("properties.gender").equalTo("male"),
-                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), female));
+                        builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), profile));
 
         // test NOT
         assertTrue(persistenceService.testMatch(
-                builder.not(builder.profileProperty("properties.gender").equalTo("male")).build(), female));
+                builder.not(builder.profileProperty("properties.gender").equalTo("male")).build(), profile));
         assertFalse(persistenceService.testMatch(
-                builder.not(builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), female));
+                builder.not(builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))).build(), profile));
 
     }
 
     @Test
     public void testDate() {
-        Date lastVisit = new Date();
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("lastVisit", lastVisit);
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
-        // test date property
+        Date lastVisit = (Date) profile.getProperty("lastVisit");
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.lastVisit").equalTo(lastVisit)
-                .build(), female));
+                .build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.profileProperty("properties.lastVisit").greaterThan(new Date(lastVisit.getTime() - 10000))
-                        .build(), female));
+                        .build(), profile));
         assertTrue(persistenceService
                 .testMatch(
                         builder.profileProperty("properties.lastVisit").lessThan(new Date(lastVisit.getTime() + 10000))
-                                .build(), female));
+                                .build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.profileProperty("properties.lastVisit")
                         .in(new Date(lastVisit.getTime() + 10000), new Date(lastVisit.getTime() - 10000), lastVisit)
-                        .build(), female));
+                        .build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.profileProperty("properties.lastVisit")
                         .notIn(new Date(lastVisit.getTime() + 10000), new Date(lastVisit.getTime() - 10000)).build(),
-                female));
+                profile));
         assertFalse(persistenceService.testMatch(
                 builder.profileProperty("properties.lastVisit")
                         .notIn(new Date(lastVisit.getTime() + 10000), new Date(lastVisit.getTime() - 10000), lastVisit)
-                        .build(), female));
+                        .build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.profileProperty("properties.lastVisit").all(new Date(lastVisit.getTime() + 10000), lastVisit)
-                        .build(), female));
+                        .build(), profile));
     }
 
     @Test
     public void testExistence() {
-        Date lastVisit = new Date();
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("lastVisit", lastVisit);
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
-        // test existence
         assertTrue("Gender property does not exist",
-                persistenceService.testMatch(builder.profileProperty("properties.gender").exists().build(), female));
+                persistenceService.testMatch(builder.profileProperty("properties.gender").exists().build(), profile));
         assertFalse("Gender property missing",
-                persistenceService.testMatch(builder.profileProperty("properties.gender").missing().build(), female));
+                persistenceService.testMatch(builder.profileProperty("properties.gender").missing().build(), profile));
         assertTrue("Strange property exists",
-                persistenceService.testMatch(builder.profileProperty("properties.unknown").missing().build(), female));
+                persistenceService.testMatch(builder.profileProperty("properties.unknown").missing().build(), profile));
         assertFalse("Strange property exists",
-                persistenceService.testMatch(builder.profileProperty("properties.unknown").exists().build(), female));
+                persistenceService.testMatch(builder.profileProperty("properties.unknown").exists().build(), profile));
     }
 
     @Test
     public void testInteger() {
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
-        // test integer property
-        assertTrue(
-
-        persistenceService.testMatch(builder.profileProperty("properties.age").equalTo(Integer.valueOf(30)).build(),
-                female));
+        assertTrue(persistenceService.testMatch(builder.profileProperty("properties.age").equalTo(Integer.valueOf(30))
+                .build(), profile));
         assertTrue(persistenceService.testMatch(
-                builder.not(builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), female));
-        assertTrue(
-
-        persistenceService.testMatch(builder.profileProperty("properties.age").notEqualTo(Integer.valueOf(40)).build(),
-                female));
-        assertTrue(
-
-        persistenceService.testMatch(builder.profileProperty("properties.age").lessThan(Integer.valueOf(40)).build(),
-                female));
-        assertTrue(
-
-        persistenceService.testMatch(
-                builder.profileProperty("properties.age").greaterThan(Integer.valueOf(20)).build(), female));
+                builder.not(builder.profileProperty("properties.age").equalTo(Integer.valueOf(40))).build(), profile));
         assertTrue(persistenceService.testMatch(
-                builder.profileProperty("properties.age").greaterThanOrEqualTo(Integer.valueOf(30)).build(), female));
+                builder.profileProperty("properties.age").notEqualTo(Integer.valueOf(40)).build(), profile));
+        assertTrue(persistenceService.testMatch(builder.profileProperty("properties.age").lessThan(Integer.valueOf(40))
+                .build(), profile));
+        assertTrue(persistenceService.testMatch(
+                builder.profileProperty("properties.age").greaterThan(Integer.valueOf(20)).build(), profile));
+        assertTrue(persistenceService.testMatch(
+                builder.profileProperty("properties.age").greaterThanOrEqualTo(Integer.valueOf(30)).build(), profile));
         assertFalse(persistenceService.testMatch(
-                builder.profileProperty("properties.age").greaterThanOrEqualTo(Integer.valueOf(31)).build(), female));
+                builder.profileProperty("properties.age").greaterThanOrEqualTo(Integer.valueOf(31)).build(), profile));
 
-        assertTrue(persistenceService.testMatch(
-                builder.profileProperty("properties.age").in(Integer.valueOf(31), Integer.valueOf(30)).build(), female));
+        assertTrue(persistenceService
+                .testMatch(builder.profileProperty("properties.age").in(Integer.valueOf(31), Integer.valueOf(30))
+                        .build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.profileProperty("properties.age").notIn(Integer.valueOf(25), Integer.valueOf(26)).build(),
-                female));
+                profile));
         assertFalse(persistenceService.testMatch(
                 builder.profileProperty("properties.age").notIn(Integer.valueOf(25), Integer.valueOf(30)).build(),
-                female));
+                profile));
     }
 
     @Test
     public void testMultiValue() {
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-        female.setSegments(new HashSet<String>(Arrays.asList("s1", "s2")));
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
         assertTrue(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "in")
-                        .parameter("segments", "s10", "s20", "s2").build(), female));
+                        .parameter("segments", "s10", "s20", "s2").build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "in")
-                        .parameter("segments", "s10", "s20", "s30").build(), female));
+                        .parameter("segments", "s10", "s20", "s30").build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "notIn")
-                        .parameter("segments", "s10", "s20", "s30").build(), female));
+                        .parameter("segments", "s10", "s20", "s30").build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "notIn")
-                        .parameter("segments", "s10", "s20", "s2").build(), female));
+                        .parameter("segments", "s10", "s20", "s2").build(), profile));
         assertTrue(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "all")
-                        .parameter("segments", "s1", "s2", "s3", "s4").build(), female));
+                        .parameter("segments", "s1", "s2", "s3", "s4").build(), profile));
         assertFalse(persistenceService.testMatch(
                 builder.property("profileSegmentCondition", "segments").parameter("matchType", "all")
-                        .parameter("segments", "s1").build(), female));
+                        .parameter("segments", "s1").build(), profile));
     }
 
     @Test
     public void testString() {
-        Date lastVisit = new Date();
-        Profile female = new Profile("profile-" + UUID.randomUUID().toString());
-        female.setProperty("lastVisit", lastVisit);
-        female.setProperty("age", Integer.valueOf(30));
-        female.setProperty("gender", "female");
-
-        ConditionBuilder builder = new ConditionBuilder(definitionsService);
-
-        // test string property
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").equalTo("female").build(),
-                female));
+                profile));
         assertFalse(persistenceService.testMatch(
-                builder.not(builder.profileProperty("properties.gender").equalTo("female")).build(), female));
+                builder.not(builder.profileProperty("properties.gender").equalTo("female")).build(), profile));
         assertTrue(persistenceService.testMatch(
-                builder.profileProperty("properties.gender").notEqualTo("male").build(), female));
+                builder.profileProperty("properties.gender").notEqualTo("male").build(), profile));
         assertFalse(
 
         persistenceService.testMatch(builder.not(builder.profileProperty("properties.gender").notEqualTo("male"))
-                .build(), female));
+                .build(), profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").startsWith("fe").build(),
-                female));
+                profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").endsWith("le").build(),
-                female));
+                profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").contains("fem").build(),
-                female));
+                profile));
         assertFalse(persistenceService.testMatch(builder.profileProperty("properties.gender").contains("mu").build(),
-                female));
+                profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").matchesRegex(".*ale")
-                .build(), female));
+                .build(), profile));
 
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").in("male", "female")
-                .build(), female));
+                .build(), profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").notIn("one", "two")
-                .build(), female));
+                .build(), profile));
         assertFalse(persistenceService.testMatch(
-                builder.profileProperty("properties.gender").notIn("one", "two", "female").build(), female));
+                builder.profileProperty("properties.gender").notIn("one", "two", "female").build(), profile));
         assertTrue(persistenceService.testMatch(builder.profileProperty("properties.gender").all("male", "female")
-                .build(), female));
+                .build(), profile));
     }
 }
