5cd06c05bad6f728392f264aeffcd9dc285ace4f
Serge Huber <shuber@jahia.com>
2015-11-11 17:15:20 +0100

    Correct all reference from 1.0-SNAPSHOT to 1.0.1-SNAPSHOT

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -3,8 +3,8 @@ echo Building...
 mvn clean install -P generate-package
 pushd package/target
 echo Uncompressing Unomi package...
-tar zxvf context-server-package-1.0-SNAPSHOT.tar.gz
-cd context-server-package-1.0-SNAPSHOT/bin
+tar zxvf context-server-package-1.0.1-SNAPSHOT.tar.gz
+cd context-server-package-1.0.1-SNAPSHOT/bin
 echo Starting Unomi...
 ./karaf debug
 popd
