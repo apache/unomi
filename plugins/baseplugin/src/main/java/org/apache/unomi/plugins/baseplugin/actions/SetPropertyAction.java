36bc5d272d8bfe101ea9ce96fc35a2b145d8d686
Abdelkader Midani <amidani@jahia.com>
2016-09-21 18:22:19 +0200

    UNOMI-56 : Add setPropertyValueMultiple in setPropertyAction.json

--- plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/actions/SetPropertyAction.java
+++ plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/actions/SetPropertyAction.java
@@ -42,11 +42,16 @@ public class SetPropertyAction implements ActionExecutor {
 
         Object propertyValue = action.getParameterValues().get("setPropertyValue");
         Object propertyValueInteger = action.getParameterValues().get("setPropertyValueInteger");
+        Object setPropertyValueMultiple = action.getParameterValues().get("setPropertyValueMultiple");
 
-        if(propertyValueInteger != null && propertyValue == null) {
-            propertyValue = PropertyHelper.getInteger(propertyValueInteger);
+        if (propertyValue == null) {
+            if (propertyValueInteger != null) {
+                propertyValue = PropertyHelper.getInteger(propertyValueInteger);
+            }
+            if(setPropertyValueMultiple != null) {
+               propertyValue = setPropertyValueMultiple;
+            }
         }
-
         if (propertyValue != null && propertyValue.equals("now")) {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
             format.setTimeZone(TimeZone.getTimeZone("UTC"));
