fb5321cbdd43065d0c7642a8408b3b8b828c594e
Serge Huber <shuber@jahia.com>
2020-11-17 12:11:52 +0100

    UNOMI-400 Fix typo bug in PropertyConditionEvaluator & improve unit test (#216)
    (cherry picked from commit 304649f515dacecd2f8f00222fd630c893a458fb)

--- plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/conditions/PropertyConditionEvaluator.java
+++ plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/conditions/PropertyConditionEvaluator.java
@@ -463,7 +463,7 @@ public class PropertyConditionEvaluator implements ConditionEvaluator {
             if ("target".equals(expression)) {
                 return event.getTarget();
             } else {
-                return getItemProperty(event.getSource(), expression.substring("target".length()+1));
+                return getItemProperty(event.getTarget(), expression.substring("target".length()+1));
             }
         }
         if ("timeStamp".equals(expression)) {
