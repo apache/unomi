18913aa7a103214ea2bee0bdc1bec49e392d4725
Pavel Milkevich <pavel.milkevich@gmail.com>
2020-12-03 12:51:01 +0300

    UNOMI-392 Integrate GraphQL v14 field visibility with always true by default

--- graphql/cxs-impl/src/main/java/org/apache/unomi/graphql/providers/GraphQLFieldVisibilityProvider.java
+++ graphql/cxs-impl/src/main/java/org/apache/unomi/graphql/providers/GraphQLFieldVisibilityProvider.java
@@ -18,6 +18,10 @@ package org.apache.unomi.graphql.providers;
 
 import graphql.schema.visibility.GraphqlFieldVisibility;
 
+/**
+ *  Provider of {@link GraphqlFieldVisibility} to limit graphql schema fields visibility
+ *  More about it here https://www.graphql-java.com/documentation/v14/fieldvisibility/
+ */
 public interface GraphQLFieldVisibilityProvider extends GraphQLProvider {
 
     GraphqlFieldVisibility getGraphQLFieldVisibility();
