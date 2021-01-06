2defa7c79ae19abc67a401351540965d9c889e50
kevan Jahanshahi <kevan@jahia.com>
2021-01-06 11:01:37 +0100

    UNOMI-412: change some DEBUG log to WARN, ERROR or INFO (#233)
    * UNOMI-412: change some DEBUG log to WARN, ERROR or INFO
    
    * UNOMI-412: code review improvements

--- plugins/request/src/main/java/org/apache/unomi/plugins/request/actions/SetRemoteHostInfoAction.java
+++ plugins/request/src/main/java/org/apache/unomi/plugins/request/actions/SetRemoteHostInfoAction.java
@@ -232,7 +232,10 @@ public class SetRemoteHostInfoAction implements ActionExecutor {
             }
             return true;
         } catch (IOException | GeoIp2Exception e) {
-            logger.debug("Cannot resolve IP", e);
+            logger.warn("Cannot resolve IP {}, enable debug log level for complete stacktrace", remoteAddr);
+            if (logger.isDebugEnabled()) {
+                logger.debug("Cannot resolve IP", e);
+            }
         }
         return false;
     }
@@ -243,7 +246,10 @@ public class SetRemoteHostInfoAction implements ActionExecutor {
             try {
                 addr = InetAddress.getByName(remoteAddr);
             } catch (UnknownHostException e) {
-                logger.debug("Cannot resolve IP", e);
+                logger.warn("Cannot resolve IP {}, enable debug log level for complete stacktrace", remoteAddr);
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Cannot resolve IP", e);
+                }
                 return false;
             }
             // Check if the address is a valid special local or loop back
--- services/src/main/java/org/apache/unomi/services/impl/events/EventServiceImpl.java
+++ services/src/main/java/org/apache/unomi/services/impl/events/EventServiceImpl.java
@@ -135,7 +135,7 @@ public class EventServiceImpl implements EventService {
                     }
                 }
             }
-            logger.debug("Could not authenticate any third party servers");
+            logger.warn("Could not authenticate any third party servers for key: {}", key);
         }
         return null;
     }
