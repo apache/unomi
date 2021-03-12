d9b9563840c125e262811a948995e1d8e6cfbbfa
Kevan <kevan@jahia.com>
2021-03-12 18:11:22 +0100

    DMF-4342: improve AllEventToProfilePropertiesAction with new optional configuration: mandatoryPropTypeSystemTag, rootProperty

--- plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/actions/AllEventToProfilePropertiesAction.java
+++ plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/actions/AllEventToProfilePropertiesAction.java
@@ -18,13 +18,16 @@
 package org.apache.unomi.plugins.baseplugin.actions;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.unomi.api.Event;
+import org.apache.unomi.api.PropertyType;
 import org.apache.unomi.api.actions.Action;
 import org.apache.unomi.api.actions.ActionExecutor;
 import org.apache.unomi.api.services.EventService;
 import org.apache.unomi.api.services.ProfileService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AllEventToProfilePropertiesAction implements ActionExecutor {
@@ -38,21 +41,25 @@ public class AllEventToProfilePropertiesAction implements ActionExecutor {
     @SuppressWarnings({"unchecked", "rawtypes"})
     public int execute(Action action, Event event) {
         boolean changed = false;
-        Map<String, Object> properties = new HashMap<String, Object>();
-        if (event.getProperties() != null) {
-            properties.putAll(event.getProperties());
-        }
+        Map<String, Object> properties = getEventPropsToCopy(action, event);
 
-
-        try {
-            Object targetProperties = BeanUtilsBean.getInstance().getPropertyUtils().getProperty(event.getTarget(), "properties");
-            if (targetProperties instanceof Map) {
-                properties.putAll((Map) targetProperties);
-            }
-        } catch (Exception e) {
-            // Ignore
-        }
+        List<String> mandatoryPropTypeSystemTags = (List<String>) action.getParameterValues().get("mandatoryPropTypeSystemTag");
         for (Map.Entry<String, Object> entry : properties.entrySet()) {
+
+            // propType Check
+            if (mandatoryPropTypeSystemTags != null && mandatoryPropTypeSystemTags.size() > 0) {
+                PropertyType propertyType = profileService.getPropertyType(entry.getKey());
+                if (propertyType == null ||
+                        propertyType.getMetadata() == null ||
+                        propertyType.getMetadata().getSystemTags() == null ||
+                        !propertyType.getMetadata().getSystemTags().containsAll(mandatoryPropTypeSystemTags)) {
+                    continue;
+                }
+            }
+
+            // TODO: handle multiple values = addValue using PropertyHelper
+            // TODO: handle single value = alwaysSet using PropertyHelper
+            // TODO: check propertyType for value type, string, long, etc...
             if (event.getProfile().getProperty(entry.getKey()) == null || !event.getProfile().getProperty(entry.getKey()).equals(event.getProperty(entry.getKey()))) {
                 String propertyMapping = profileService.getPropertyTypeMapping(entry.getKey());
                 String propertyName = (propertyMapping != null) ? propertyMapping : entry.getKey();
@@ -61,5 +68,34 @@ public class AllEventToProfilePropertiesAction implements ActionExecutor {
             }
         }
         return changed ? EventService.PROFILE_UPDATED : EventService.NO_CHANGE;
+    }
+
+    private Map<String, Object> getEventPropsToCopy(Action action, Event event) {
+        Map<String, Object> propsToCopy = new HashMap<String, Object>();
+
+        String rootProperty = (String) action.getParameterValues().get("rootProperty");
+        boolean copyEventProps = false;
+
+        if (rootProperty == null || rootProperty.length() == 0) {
+            copyEventProps = true;
+            rootProperty = "target.properties";
+        }
+
+        // copy props from the event.properties
+        if (copyEventProps && event.getProperties() != null) {
+            propsToCopy.putAll(event.getProperties());
+        }
+
+        // copy props from the specified level (default is: target.properties)
+        try {
+            Object targetProperties = BeanUtilsBean.getInstance().getPropertyUtils().getProperty(event, rootProperty);
+            if (targetProperties instanceof Map) {
+                propsToCopy.putAll((Map) targetProperties);
+            }
+        } catch (Exception e) {
+            // Ignore
+        }
+
+        return propsToCopy;
     }
 }
