82a9afb16194c8286fc12dafdd1705195945c4f1
Taybou <benterki.tayeb@gmail.com>
2021-04-08 11:37:18 +0200

    UNOMI-449 deprecate servlets

--- wab/src/main/java/org/apache/unomi/web/ClientServlet.java
+++ wab/src/main/java/org/apache/unomi/web/ClientServlet.java
@@ -29,8 +29,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * @deprecated please use the rest endpoint {@link org.apache.unomi.rest.ClientEndpoint} instead of this servlet
  * A servlet filter to serve a context-specific Javascript containing the current request context object.
  */
+@Deprecated
 public class ClientServlet extends HttpServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(ClientServlet.class.getName());
