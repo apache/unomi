978929f69ef7bd69bcf236f554322ac5db34cba0
Kevan <kevan@jahia.com>
2022-11-17 13:54:44 +0100

    UNOMI-691: fix personalization strategy status stored on profile during merge

--- itests/src/test/java/org/apache/unomi/itests/ProfileMergeIT.java
+++ itests/src/test/java/org/apache/unomi/itests/ProfileMergeIT.java
@@ -24,6 +24,7 @@ import org.apache.unomi.api.conditions.Condition;
 import org.apache.unomi.api.rules.Rule;
 import org.apache.unomi.api.services.DefinitionsService;
 import org.apache.unomi.api.services.EventService;
+import org.apache.unomi.api.services.ProfileService;
 import org.apache.unomi.api.services.RulesService;
 import org.junit.After;
 import org.junit.Assert;
@@ -35,9 +36,7 @@ import org.ops4j.pax.exam.spi.reactors.PerSuite;
 import org.ops4j.pax.exam.util.Filter;
 
 import javax.inject.Inject;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Integration test for MergeProfilesOnPropertyAction
@@ -45,6 +44,10 @@ import java.util.HashMap;
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerSuite.class)
 public class ProfileMergeIT extends BaseIT {
+    public static final String PERSONALIZATION_STRATEGY_STATUS = "personalizationStrategyStatus";
+    public static final String PERSONALIZATION_STRATEGY_STATUS_ID = "personalizationId";
+    public static final String PERSONALIZATION_STRATEGY_STATUS_IN_CTRL_GROUP = "inControlGroup";
+    public static final String PERSONALIZATION_STRATEGY_STATUS_DATE = "timeStamp";
 
     @Inject @Filter(timeout = 600000)
     protected EventService eventService;
@@ -52,6 +55,8 @@ public class ProfileMergeIT extends BaseIT {
     protected RulesService rulesService;
     @Inject @Filter(timeout = 600000)
     protected DefinitionsService definitionsService;
+    @Inject @Filter(timeout = 600000)
+    protected ProfileService profileService;
 
     private final static String TEST_EVENT_TYPE = "mergeProfileTestEventType";
     private final static String TEST_RULE_ID = "mergeOnPropertyTest";
@@ -79,6 +84,79 @@ public class ProfileMergeIT extends BaseIT {
         Assert.assertEquals(sendEvent().getProfile().getItemId(), TEST_PROFILE_ID);
     }
 
+    @Test
+    public void testPersonalizationStrategyStatusMerge() {
+        // create some statuses for the tests:
+        Map<String, Object> perso1true = buildPersonalizationStrategyStatus("perso-test-1", true);
+        Map<String, Object> perso1false = buildPersonalizationStrategyStatus("perso-test-1", false);
+        Map<String, Object> perso2false = buildPersonalizationStrategyStatus("perso-test-2", false);
+        Map<String, Object> perso3true = buildPersonalizationStrategyStatus("perso-test-3", true);
+
+        // create a single master profile we will keep along all the tests:
+        Profile profileMaster = new Profile("master-profile");
+
+        // merge test 1: Master do not have statuses, but slave have statuses
+        // master have: NULL
+        // slave have:  perso-test-1 -> true
+        //              perso-test-2 -> false
+        // Expected:    perso-test-1 -> true
+        //              perso-test-2 -> false
+        Profile profileSlave = new Profile("slave-profile");
+        profileSlave.setSystemProperty(PERSONALIZATION_STRATEGY_STATUS, new ArrayList<>(Arrays.asList(perso1true, perso2false)));
+        profileMaster = profileService.mergeProfiles(profileMaster, Collections.singletonList(profileSlave));
+        assertPersonalizationStrategyStatus(profileMaster,  Arrays.asList(perso1true, perso2false));
+
+        // merge test 2: Master do not have statuses, but slave have statuses
+        // master have: perso-test-1 -> true
+        //              perso-test-2 -> false
+        // slave have:  NULL
+        // Expected:    perso-test-1 -> true
+        //              perso-test-2 -> false
+        profileSlave = new Profile("slave-profile");
+        profileMaster = profileService.mergeProfiles(profileMaster, Collections.singletonList(profileSlave));
+        assertPersonalizationStrategyStatus(profileMaster, Arrays.asList(perso1true, perso2false));
+
+        // merge test 3: both master and slave have strategy statuses and some are conflicting
+        // master have: perso-test-1 -> true
+        //              perso-test-2 -> false
+        // slave have:  perso-test-1 -> false (conflicting)
+        //              perso-test-3 -> true
+        // Expected:    perso-test-1 -> true
+        //              perso-test-2 -> false
+        //              perso-test-3 -> true
+        profileSlave = new Profile("slave-profile");
+        profileSlave.setSystemProperty(PERSONALIZATION_STRATEGY_STATUS, new ArrayList<>(Arrays.asList(perso1false, perso3true)));
+        profileMaster = profileService.mergeProfiles(profileMaster, Collections.singletonList(profileSlave));
+        assertPersonalizationStrategyStatus(profileMaster,  Arrays.asList(perso1true, perso2false, perso3true));
+    }
+
+    private Map<String, Object> buildPersonalizationStrategyStatus(String persoId, boolean inControlGroup) {
+        Map<String, Object> personalizationStrategyStatus = new HashMap<>();
+        personalizationStrategyStatus.put(PERSONALIZATION_STRATEGY_STATUS_ID, persoId);
+        personalizationStrategyStatus.put(PERSONALIZATION_STRATEGY_STATUS_DATE, new Date());
+        personalizationStrategyStatus.put(PERSONALIZATION_STRATEGY_STATUS_IN_CTRL_GROUP, inControlGroup);
+        return personalizationStrategyStatus;
+    }
+
+    private void assertPersonalizationStrategyStatus(Profile profile, List<Map<String, Object>> expectedStatuses) {
+        List<Map<String, Object>> strategyStatuses = (List<Map<String, Object>>) profile.getSystemProperties().get(PERSONALIZATION_STRATEGY_STATUS);
+        Assert.assertEquals("We didn't get the good number of expected personalization statuses on the given profile: " + profile.getItemId(),
+                expectedStatuses.size(), strategyStatuses.size());
+        for (Map<String, Object> expectedStatus : expectedStatuses) {
+            Optional<Map<String, Object>> foundStatusOp = strategyStatuses
+                    .stream()
+                    .filter(strategyStatus -> strategyStatus.get(PERSONALIZATION_STRATEGY_STATUS_ID)
+                            .equals(expectedStatus.get(PERSONALIZATION_STRATEGY_STATUS_ID)))
+                    .findFirst();
+            if (foundStatusOp.isPresent()) {
+                Map<String, Object> foundStatus = foundStatusOp.get();
+                Assert.assertEquals(expectedStatus.get(PERSONALIZATION_STRATEGY_STATUS_DATE), foundStatus.get(PERSONALIZATION_STRATEGY_STATUS_DATE));
+                Assert.assertEquals(expectedStatus.get(PERSONALIZATION_STRATEGY_STATUS_IN_CTRL_GROUP), foundStatus.get(PERSONALIZATION_STRATEGY_STATUS_IN_CTRL_GROUP));
+            } else {
+                Assert.fail("We didn't found the expected personalization: " + expectedStatus.get(PERSONALIZATION_STRATEGY_STATUS_ID) + " status on the given profile: " + profile.getItemId());
+            }
+        }
+    }
     private Event sendEvent() {
         Profile profile = new Profile();
         profile.setProperties(new HashMap<>());
