450d525e1de71cf61f893c696258507317373061
Serge Huber <shuber@jahia.com>
2022-12-07 17:23:07 +0100

    Backport UNOMI-721 & UNOMI-126

--- generate-site-and-upload.sh
+++ generate-site-and-upload.sh
@@ -17,16 +17,47 @@
 #    limitations under the License.
 #
 ################################################################################
-if [ $# -ne 2 ]
+if [ $# -ne 4 ]
   then
-    echo "Illegal number of arguments supplied. Syntax should be generate-site-and-upload.sh SVNusername SVNpassword"
+    echo "Illegal number of arguments supplied. Syntax should be generate-site-and-upload.sh X_X_X X.X.X SVNusername SVNpassword "
+    echo "Example: ./generate-site-and-upload.sh 2_0_x 2.0.1 user password"
     exit 1
 fi
-echo Generating site...
-mvn clean install site site:stage -P !integration-tests,!performance-tests
-cd rest
-mvn package
-cd -
-echo Committing site to Apache SVN...
-mvn scm-publish:publish-scm -Dusername=$1 -Dpassword=$2
-echo Site generation and upload completed.
+echo Setting up environment...
+DIRNAME=`dirname "$0"`
+PROGNAME=`basename "$0"`
+if [ -f "$DIRNAME/setenv.sh" ]; then
+  . "$DIRNAME/setenv.sh"
+fi
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+BRANCH_NAME=$1
+VERSION=$2
+SVN_USERNAME=$3
+SVN_PASSWORD=$4
+LOCAL_BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+echo Git local branch: ${LOCAL_BRANCH_NAME}
+bash generate-site.sh $BRANCH_NAME $VERSION
+echo Committing documentation to Apache SVN...
+mvn scm-publish:publish-scm -Dscmpublish.pubScmUrl=scm:svn:https://svn.apache.org/repos/asf/unomi/website/manual -Dscmpublish.content=target/staging/manual -Dusername=$SVN_USERNAME -Dpassword=$SVN_PASSWORD
+if [ "$LOCAL_BRANCH_NAME" == "master" ]; then
+  mvn scm-publish:publish-scm -Dscmpublish.pubScmUrl=scm:svn:https://svn.apache.org/repos/asf/unomi/website/unomi-api -Dscmpublish.content=target/staging/unomi-api -Dusername=$SVN_USERNAME -Dpassword=$SVN_PASSWORD
+fi
+
+echo "Committing manual to Apache Dist SVN..."
+pushd manual/target
+svn co https://dist.apache.org/repos/dist/release/unomi/$VERSION
+mv unomi-manual-$BRANCH_NAME.pdf $VERSION
+mv unomi-manual-$BRANCH_NAME.pdf.asc $VERSION
+mv unomi-manual-$BRANCH_NAME.zip $VERSION
+mv unomi-manual-$BRANCH_NAME.pdf.sha512 $VERSION
+mv unomi-manual-$BRANCH_NAME.zip.asc $VERSION
+mv unomi-manual-$BRANCH_NAME.zip.sha512 $VERSION
+cd $VERSION
+svn add unomi-manual*
+svn commit -m "Update Unomi manual packages for version ${VERSION}"
+popd
+echo Documentation generation and upload completed.
--- generate-site.sh
+++ generate-site.sh
@@ -17,8 +17,46 @@
 #    limitations under the License.
 #
 ################################################################################
-echo Generating site...
-mvn site site:stage -P !integration-tests,!performance-tests
-cd rest
-mvn package
-cd -
+if [ $# -ne 2 ]
+  then
+    echo "Illegal number of arguments supplied. Syntax should be generate-site.sh X_X_X X.X.X"
+    echo "Example: ./generate.sh 2_0_x 2.0.1"
+    exit 1
+fi
+echo Setting up environment...
+DIRNAME=`dirname "$0"`
+PROGNAME=`basename "$0"`
+if [ -f "$DIRNAME/setenv.sh" ]; then
+  . "$DIRNAME/setenv.sh"
+fi
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+RELEASE_BRANCH_NAME=$1
+RELEASE_VERSION=$2
+LOCAL_BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+echo Git local branch: ${LOCAL_BRANCH_NAME}
+echo Generating manual for branch ${RELEASE_BRANCH_NAME} and version ${RELEASE_VERSION}...
+mvn clean
+pushd manual
+mvn -Ddoc.archive=true -Ddoc.output.pdf=target/generated-docs/pdf/$RELEASE_BRANCH_NAME -Ddoc.output.html=target/generated-docs/html/$RELEASE_BRANCH_NAME -Ddoc.version=$RELEASE_BRANCH_NAME -P sign install
+mvn -P sign install
+# If not on master branch we remove the latest directories
+if [ "$LOCAL_BRANCH_NAME" != "master" ]; then
+  rm -rf target/generated-docs/html/latest
+  rm -rf target/generated-docs/pdf/latest
+fi
+popd
+if [ "$LOCAL_BRANCH_NAME" == "master" ]; then
+  echo Generating Javadoc...
+  mvn javadoc:aggregate -P integration-tests
+else
+  echo Not on master branch, skipping Javadoc generation
+fi
+mkdir -p target/staging/unomi-api
+mkdir -p target/staging/manual
+cp -R target/site/apidocs target/staging/unomi-api
+cp -Rf manual/target/generated-docs/html/* target/staging/manual
+echo Documentation generation completed!
