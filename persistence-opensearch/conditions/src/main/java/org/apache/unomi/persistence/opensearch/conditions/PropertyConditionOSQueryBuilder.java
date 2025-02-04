e4d021bf9f2e2dd6123bc52a9d6f96edfd21525a
Serge Huber <shuber@jahia.com>
2025-02-04 11:19:52 +0100

    Update persistence-opensearch/conditions/src/main/java/org/apache/unomi/persistence/opensearch/conditions/PropertyConditionOSQueryBuilder.java

--- persistence-opensearch/conditions/src/main/java/org/apache/unomi/persistence/opensearch/conditions/PropertyConditionOSQueryBuilder.java
+++ persistence-opensearch/conditions/src/main/java/org/apache/unomi/persistence/opensearch/conditions/PropertyConditionOSQueryBuilder.java
@@ -176,7 +176,7 @@ public class PropertyConditionOSQueryBuilder implements ConditionOSQueryBuilder 
 
     private void checkRequiredValuesSize(Collection<?> values, String name, String operator, int expectedSize) {
         if (values == null || values.size() != expectedSize) {
-            throw new IllegalArgumentException("Impossible to build ES filter, missing " + expectedSize + " values for a condition using comparisonOperator: " + operator + ", and propertyName: " + name);
+            throw new IllegalArgumentException("Impossible to build OS filter, missing " + expectedSize + " values for a condition using comparisonOperator: " + operator + ", and propertyName: " + name);
         }
     }
 
