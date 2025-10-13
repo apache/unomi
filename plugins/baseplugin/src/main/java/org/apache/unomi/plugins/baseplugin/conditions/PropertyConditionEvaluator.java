ddfbf1e6b39d18004f6601a7c4970bc4bbab033d
Serge Huber <shuber@jahia.com>
2025-10-13 20:43:42 +0200

    Refactor PropertyConditionEvaluator to use foldToASCII for string comparison, aligning with analyzer configuration behavior

--- plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/conditions/PropertyConditionEvaluator.java
+++ plugins/baseplugin/src/main/java/org/apache/unomi/plugins/baseplugin/conditions/PropertyConditionEvaluator.java
@@ -9,13 +9,13 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
-        * distributed under the License is distributed on an "AS IS" BASIS,
-        * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        * See the License for the specific language governing permissions and
-        * limitations under the License.
-        */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-        package org.apache.unomi.plugins.baseplugin.conditions;
+package org.apache.unomi.plugins.baseplugin.conditions;
 
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -80,8 +80,8 @@ public class PropertyConditionEvaluator implements ConditionEvaluator {
         } else if (expectedValueDateExpr != null) {
             return getDate(actualValue).compareTo(getDate(expectedValueDateExpr));
         } else {
-            // We use toLowerCase here to match the behavior of the analyzer configuration in the persistence configuration
-            return actualValue.toString().toLowerCase().compareTo(expectedValue);
+            // We use foldToASCII here to match the behavior of the analyzer configuration in the persistence configuration
+            return ConditionContextHelper.foldToASCII(actualValue.toString()).compareTo(expectedValue);
         }
     }
 
