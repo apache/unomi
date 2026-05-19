958d2fb2f6fecbc4de35addda302eb9f8927a237
Serge Huber <shuber@apache.org>
2026-05-19 16:53:25 +0200

    UNOMI-139: Fix TenantIT and EventsCollectorIT integration failures
    - TenantIT: use /cxs/context.json (REST endpoint) instead of legacy
      /context.json servlet forward that timed out under Pax Exam.
    - EventCollectorResponse: add Jackson @JsonCreator so IT clients can
      deserialize the POST /cxs/eventcollector JSON body.

--- rest/src/main/java/org/apache/unomi/rest/models/EventCollectorResponse.java
+++ rest/src/main/java/org/apache/unomi/rest/models/EventCollectorResponse.java
@@ -17,12 +17,16 @@
 
 package org.apache.unomi.rest.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class EventCollectorResponse implements Serializable {
     private int updated;
 
-    public EventCollectorResponse(int updated) {
+    @JsonCreator
+    public EventCollectorResponse(@JsonProperty("updated") int updated) {
         this.updated = updated;
     }
 
